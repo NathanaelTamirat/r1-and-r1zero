1ffc655b5ca0c18b88fdd6ee66a472f73cbab38a
Nathanael <natnaeltamirat12@gmail.com>
2025-03-18 16:28:16 +0800

    Added xml tag counting and lenght reward

--- r1zero_r1/reward_function.py
+++ r1zero_r1/reward_function.py
@@ -1,8 +1,9 @@
 import re
 import math
-from transformers.trainer_utils import get_last_checkpoint
+from typing import Dict
 from latex2sympy2_extended import NormalizationConfig
 from math_verify import LatexExtractionConfig, parse, verify
+
 
 ###accuracy reward
 """
@@ -21,7 +22,9 @@ def accuracy_reward(completions,**kwargs):
         return [0.5] * len(completions) #netural reward if no solution
     for content, sol in zip(contents,solutions):
         #parse the ground truth solution
-        gold_parsed=parse(sol,extraction_node="first_match",extraction_config=LatexExtractionConfig())
+        gold_parsed=parse(sol,
+                          extraction_node="first_match",
+                          extraction_config=LatexExtractionConfig())
         if len(gold_parsed) != 0:
             answer_parsed=parse( #parse the models answer
                 content,
@@ -52,6 +55,7 @@ def accuracy_reward(completions,**kwargs):
         rewards.append(reward)
     return rewards
 
+
 ###format reward
 """
 1. We define a regular expression (regex) pattern that specifies the expected structure: the content should begin with a certain part, contain anything until a specific point, include spaces, continue with another section, have more content until another marker, and then end precisely there.
@@ -64,6 +68,7 @@ def format_reward(completions,**kwargs):
     completion_contents=[completion[0]["content"] for completion in completions]
     matches=[re.match(pattern, content ,re.DOTALL | re.MULTILINE) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
+
 
 ###reasoning step reward
 """
@@ -79,6 +84,7 @@ def reasoning_steps_reward(completions,**kwargs):
     completion_content=[completion[0]["content"] for completion in completions]
     matches=[re.match(pattern,content,re.MULTILINE) for content in completion_content]
     return [min(1.0 ,count / 3 ) for count in matches] #encouraging atleast 3 steps
+
 
 ###Cosine Scaled reward
 """ 
@@ -109,9 +115,10 @@ def get_cosine_scaled_reward(min_value_wrong: float=-0.5,
         return rewards
     return cosine_scaled_reward
 
+
 ###repetition penality reward
 """ 
-discouraging our model from getting stuck in the loop
+discouraging the model from getting stuck in the loop
 if it uses the same seq of words(ngram) too many times
 """
 
@@ -141,3 +148,82 @@ def get_repetition_penaltiy_reward(ngram_size: int=3, max_penalty: float =-0.1):
             rewards.append(reward)
         return rewards
     return repetition_penalty_reward
+
+
+###xml tag counting reward
+"""
+this checks if we generate the exact number of <think> and <answer> xml tag
+its quite similar with the foramat reward.
+"""
+
+def xml_count_reward(completions,**kwarg):
+    def count_xml(text: str) -> float:
+        count=0.0
+        if text.count("<think>\n") ==1:
+            count +=0.25
+        if text.count("\n</think>\n") ==1:
+            count += 0.25
+        if text.count("\n<answer>\n")==1:
+            count += 0.25
+        if text.count("\n</answer>") ==1:
+            count += 0.25
+        return count
+    contents=[completion[0]["content"] for completion in completions]
+    return [count_xml(c) for c in contents]
+
+
+#length of reward
+"""
+dicourages overthinking during generation
+generate token efficently 
+"""
+
+def len_reward(completions: list[Dict[str,str]],solution: list[str],**kwargs) -> float:
+    contents=[completion[0]["content"] for completion in completions]
+    correctness=[]
+    for content, sol in zip(contents,solution):
+        gold_parse=parse(
+            sol,
+            extraction_config=[LatexExtractionConfig()],
+            extraction_mode="first_match"
+        )
+        if len(gold_parse)==0:
+            correctness.append(True) #skip unparsable examples by treating them as a true value
+            print("failed to parse gold solution",sol)
+            continue
+        answer_parse=parse(
+            content,
+            extraction_mode="first_match",
+            extraction_config=[
+                LatexExtractionConfig(
+                    boxed_match_priority=0,
+                    try_extract_without_anchor=False,
+                    normalization_config=NormalizationConfig(
+                        nits=False,
+                        malformed_operators=False,
+                        basic_latex=True,
+                        equations=True,
+                        boxed=True,
+                        units=True
+                    )
+                )
+            ]
+        )
+        correctness.append(verify(answer_parse,gold_parse))
+    lengths=[len(content) for content in contents]
+    min_len=min(lengths)
+    max_len=max(lengths)
+
+    if max_len == min_len:
+        return [0.0 * len(completions)] #just return zero
+    rewards=[]
+    for length,is_correct in zip(length,correctness):
+        lambda_val=0.5-(length-min_len)/(max_len-min_len) #scaling
+        if is_correct:
+            reward=lambda_val
+        else:
+            reward=min(0,lambda_val)
+        rewards.append(float(reward))
+    return rewards
+
+#TODO implement reward for generated code.
